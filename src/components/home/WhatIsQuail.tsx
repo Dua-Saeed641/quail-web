2dd89f066dc5881ff1ef7f07cb667a59a2b3ed70
Dua-Saeed641 <duasaeed641@gmail.com>
2026-08-24 21:42:46 +0530

    feat: added introduction section

--- src/components/home/WhatIsQuail.tsx
+++ src/components/home/WhatIsQuail.tsx
@@ -1,6 +1,35 @@
+import "./WhatIsQuail.css";
+
 function WhatIsQuail() {
   return (
-    <section id="what-is-quail"></section>
+    <section id="what-is-quail" className="what-is-quail">
+      <div className="what-is-quail__content">
+        <div className="what-is-quail__label">
+          <span>01</span>
+          <span>WHAT IS QUAIL?</span>
+        </div>
+
+        <div className="what-is-quail__body">
+          <h2>
+            Email designed for
+            <br />
+            a changing security landscape.
+          </h2>
+
+          <p>
+            Quail is a secure email platform designed to explore
+            quantum-resilient communication while preserving the familiar
+            experience of email.
+          </p>
+
+          <p>
+            Our approach combines established email infrastructure with
+            an additional security layer designed to protect the
+            confidentiality of messages and their associated data.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
 
