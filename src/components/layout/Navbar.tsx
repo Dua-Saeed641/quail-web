09749aa0e30c436b65c1480775d0e14a0d7da0b8
Dua-Saeed641 <duasaeed641@gmail.com>
2026-08-24 21:06:01 +0530

    feat:implemented nav bar layout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,23 @@
-function Terms() {
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
   return (
-    <main>
-      <h1>Terms of Service</h1>
-      <p>Please review the terms governing the use of Quail.</p>
-    </main>
+    <header>
+      <nav className="navbar">
+        <Link to="/" className="navbar__brand">
+          QUAIL
+        </Link>
+
+        <div className="navbar__links">
+          <Link to="/">Home</Link>
+          <Link to="/get-verified">Get Verified</Link>
+          <Link to="/privacy">Privacy</Link>
+          <Link to="/terms">Terms</Link>
+        </div>
+      </nav>
+    </header>
   );
 }
 
-export default Terms;
+export default Navbar;
