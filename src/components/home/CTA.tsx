a44bd67792b3caa6700df60d7ce2486b7b791829
Dua-Saeed641 <duasaeed641@gmail.com>
2026-08-24 21:51:24 +0530

    feat:landing page completed

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import Button from "../common/Button";
+import "./CTA.css";
+
 function CTA() {
   return (
-    <section>
-      <h1>CTA</h1>
+    <section className="cta">
+      <div className="cta__content">
+        <p className="cta__eyebrow">GET STARTED</p>
+
+        <h2>
+          Ready to explore
+          <br />
+          Quail?
+        </h2>
+
+        <p className="cta__description">
+          Start by verifying your identity and learn how Quail's secure
+          communication environment works.
+        </p>
+
+        <Link to="/get-verified">
+          <Button>Get Verified</Button>
+        </Link>
+      </div>
     </section>
   );
 }
